0c1f9e170e442f0ae0eb2e59ee526d3a248ce962
kubaorlowski <orlowskijakub1@gmail.com>
2017-10-02 15:26:48 +0200

    cw2.1

---
+++ cwiczenie2/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <windows.h>
+
+using namespace std;
+
+
+void gotoxy(int x, int y)
+{
+	COORD coord = {x, y};
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+	return;
+}
+
+
+int main(int argc, char*argv[])
+{
+    int wiersze;
+    cout<<"Podaj liczbe wierszy : "<<endl;
+
+    cin>>wiersze;
+    for (int i=1;i<=wiersze; i++)
+    {
+        for(int j=1 ;j<=(wiersze-i)+15 ;j++)
+        {
+
+           cout<<" ";
+        }
+        for(int k=1 ;k<=(2*i)-1 ;k++)
+        {
+
+            cout<<"*";
+        }
+
+            cout<<" "<<endl;
+
+    }
+
+    return 0;
+}
